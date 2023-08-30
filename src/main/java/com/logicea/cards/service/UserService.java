0ca53c43df1c3130a352be6ab987334602cd24fb
chris <chrisadriane.ca@gmail.com>
2023-08-30 13:24:19 +0300

    update

--- src/main/java/com/logicea/cards/service/UserService.java
+++ src/main/java/com/logicea/cards/service/UserService.java
@@ -2,23 +2,54 @@ package com.logicea.cards.service;
 
 import com.logicea.cards.model.UserEntity;
 
+import java.util.List;
+
 public interface UserService {
-    
+
     /**
-     * Retrieves a user by their unique ID.
-     * 
-     * @param userId the unique ID of the user.
-     * @return the user entity, or null if not found.
+     * Authenticate a user with their email and password.
+     *
+     * @param email        Email of the user.
+     * @param rawPassword  Raw password provided by the user.
+     * @return A JWT token for valid users.
+     */
+    String authenticate(String email, String rawPassword);
+
+    /**
+     * Register a new user.
+     *
+     * @param user  User details.
+     * @return Registered user entity.
+     */
+    UserEntity register(UserEntity user);
+
+    /**
+     * Update details of an existing user.
+     *
+     * @param updatedUser  Updated user details.
+     * @return Updated user entity.
+     */
+    UserEntity updateUser(UserEntity updatedUser);
+
+    /**
+     * Fetch all users in the system.
+     *
+     * @return List of all users.
+     */
+    List<UserEntity> getAllUsers();
+
+    /**
+     * Fetch a user by their ID.
+     *
+     * @param userId  ID of the user.
+     * @return User entity.
      */
     UserEntity getUserById(Long userId);
 
     /**
-     * Retrieves a user by their email address.
-     * 
-     * @param email the email address of the user.
-     * @return the user entity, or null if not found.
+     * Delete a user based on their ID.
+     *
+     * @param userId  ID of the user.
      */
-    UserEntity getUserByEmail(String email);
-    
-    // Any other user-related methods can be added as required.
+    void deleteUser(Long userId);
 }
--- src/main/java/com/logicea/cards/service/impl/UserServiceImpl.java
+++ src/main/java/com/logicea/cards/service/impl/UserServiceImpl.java
@@ -1,26 +1,80 @@
 package com.logicea.cards.service.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import com.logicea.cards.exceptions.ResourceNotFoundException;
 import com.logicea.cards.model.UserEntity;
 import com.logicea.cards.repository.UserRepository;
+import com.logicea.cards.security.JwtTokenProvider;
 import com.logicea.cards.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
-    
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
     @Override
+    public String authenticate(String email, String rawPassword) {
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(email, rawPassword)
+            );
+            UserEntity userEntity = userRepository.findByEmail(email);
+            if (userEntity == null || !passwordEncoder.matches(rawPassword, userEntity.getPassword())) {
+                throw new BadCredentialsException("Invalid email or password");
+            }
+            return jwtTokenProvider.generateToken(userEntity.getEmail());
+        } catch (AuthenticationException e) {
+            throw new BadCredentialsException("Invalid email or password", e);
+        }
+    }
+
+    public UserEntity register(UserEntity user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public UserEntity updateUser(UserEntity updatedUser) {
+        Optional<UserEntity> existingUser = userRepository.findById(updatedUser.getId());
+        if (!existingUser.isPresent()) {
+            throw new ResourceNotFoundException("User not found with ID: " + updatedUser.getId());
+        }
+        if (updatedUser.getPassword() != null) {
+            updatedUser.setPassword(passwordEncoder.encode(updatedUser.getPassword()));
+        }
+        return userRepository.save(updatedUser);
+    }
+
+    public List<UserEntity> getAllUsers() {
+        return userRepository.findAll();
+    }
+
     public UserEntity getUserById(Long userId) {
-        return userRepository.findById(userId).orElse(null);
+        return userRepository.findById(userId)
+            .orElseThrow(() -> new ResourceNotFoundException("User not found with ID: " + userId));
     }
 
-    @Override
-    public UserEntity getUserByEmail(String email) {
-        return userRepository.findByEmail(email);
+    public void deleteUser(Long userId) {
+        if (!userRepository.existsById(userId)) {
+            throw new ResourceNotFoundException("User not found with ID: " + userId);
+        }
+        userRepository.deleteById(userId);
     }
-
-    // Implement any other user-related methods as required.
 }
